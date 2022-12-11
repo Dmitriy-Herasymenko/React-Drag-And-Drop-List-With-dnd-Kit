946da913b6aea9cbf0e3636e16829cb6e4fc0a2f
Dmitriy Herasymenko <shiningxors@gmail.com>
2022-12-11 12:16:06 +0200

    add style sortable items amd mapping

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from "react";
-import { Container } from "react-bootstrap";
+import Container from 'react-bootstrap/Container';
 import {
   DndContext,
   closestCenter
@@ -9,21 +13,25 @@ import {
   SortableContext,
   verticalListSortingStrategy
 } from "@dnd-kit/sortable";
+import {SortableItem} from "./SortableItem";
+
 function App() {
-  const [lang, setLang] = useState(["JavaScritp, Python, PHP, TypeScript"])
+  const [lang, setLang] = useState(["JavaScritp", "Python", "PHP", "TypeScript"])
   return (
-   <DndContext
-    collisionDetection={closestCenter}
-    onDragEnd={handleDragEnd}
-   >
-    <Container className="p-3" style={{"width": "50%"}} align="center">
-      <h3>Best lang prog</h3>
-      <SortableContext>
-        items={lang}
-        strategy={verticalListSortingStrategy}
-      </SortableContext>
-    </Container>
-  </DndContext>
+    <DndContext
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+    >
+      <Container className="p-3" style={{"width": "50%"}} align="center">
+        <h3>The best programming languages!</h3>
+        <SortableContext
+          items={lang}
+          strategy={verticalListSortingStrategy}
+        >
+          {lang.map(language => <SortableItem key={language} id={language}/>)}
+        </SortableContext>
+      </Container>
+    </DndContext>
   );
 
   function handleDragEnd (event) {
--- src/SortableItem.js
+++ src/SortableItem.js
@@ -6,10 +6,16 @@ export function SortableItem(props) {
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id: props.id });
 
-  const style = {
-    transform: CSS.Transform.toString(transform),
-    transition,
-  };
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition
+    }
 
-  return <div ref={setNodeRef} style={style} {...attributes}></div>;
+  return (
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+      <Card body className="m-3">
+        {props.id}
+      </Card>
+    </div>
+  );
 }
